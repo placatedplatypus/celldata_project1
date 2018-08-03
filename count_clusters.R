5d036c91fa422a84c2e1e9d34430a56adb6b2e9d
placatedplatypus <40672430+placatedplatypus@users.noreply.github.com>
2018-08-03 15:03:37 -0700

    Symbolic link added
    Now instead of taking from the full path, the data csv is grabbed from a symbolic link to it created in UNIX.

--- count_clusters.R
+++ count_clusters.R
@@ -1,9 +1,8 @@
-# our working directory is /home/sereno/projects/cell_clustering
-# we need to be in /home/vagar/projects/forSereno/ [files]
+# our working directory is /net/shendure/vol10/projects/cell_clustering
+# files are in /net/shendure/vol1/home/vagar/projects/forSereno/ [files], sym linked to wd/data/
 library(data.table)
-PATH <- "../../../vagar/projects/forSereno"
 
-pathcsv <- paste(PATH,"/df_cell_cluster_1.csv",sep = "") ## need sep = "" to stop it from inserting space
+pathcsv <- "data/df_cell_cluster_1.csv"
 
 DT <- data.table(read.csv(file = pathcsv, header = TRUE, colClasses = c("NULL","character","NULL")))
 # returns one line data table of cell types
